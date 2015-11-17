e3f3ed7833dcfceb33c4a92d9bdc34c48130c497
Marc Scott <marc@raspberrypi.org>
2015-11-17 19:07:10 +0000

    update

--- lesson-5/code/sorting.py
+++ lesson-5/code/sorting.py
@@ -6,9 +6,6 @@ from time import sleep
 ## Interactive graph plotting
 plt.ion()
 
-
-
-
 def create_random_list(length):
     '''create a random list of given length'''
     some_list = [i for i in range(length)]
@@ -20,9 +17,6 @@ def display(some_list):
     plt.clf()
     plt.scatter(range(len(some_list)),some_list)
     plt.draw()
-
-
-
 
 def my_bubble_sort(some_list):
     swapped = True
@@ -57,10 +51,109 @@ def my_selection_sort(some_list):
 
     return some_list
 
-tree = []
-parent = 1
-def my_quick_sort(some_list, level=0):
-    global parent
+#my_bubble_sort(create_random_list(250))
+# #my_insertion_sort(create_random_list(250))
+# #my_selection_sort(create_random_list(250))
+
+# tree = []
+# parent = 1
+
+# def my_quick_sort(some_list):
+#     global parent
+#     small = []
+#     equal = []
+#     large = []
+#     if len(some_list) > 1:
+#         pivot = some_list[len(some_list)//2]
+#         for i in some_list:
+#             if i > pivot:
+#                 large.append(i)
+#             elif i < pivot:
+#                 small.append(i)
+#             else:
+#                 equal.append(i)
+#         tree.append({'parent':parent,'data':small})
+#         tree.append({'parent':parent,'data':equal})
+#         tree.append({'parent':parent,'data':large})
+#         parent += 1
+#         new_list = my_quick_sort(small) + my_quick_sort(equal) + my_quick_sort(large)
+#         #display(some_list) ##Visualising what is happening but not exactly clear
+#         #tree.append([level, new_list])
+#         return new_list
+#     else:
+#         #tree.append([level, some_list])
+#         return some_list     
+
+# #my_bubble_sort(create_random_list(250))
+# #my_insertion_sort(create_random_list(250))
+# #my_selection_sort(create_random_list(250))
+# data = create_random_list(50)
+# tree.append({'parent':0,'data':data})
+# my_quick_sort(data)
+
+# count = 0
+# hold = [i['data'] for i in tree if i['parent'] == count]        
+
+# class Node(object):
+#     def __init__(self, parent):
+#         self.parent = parent
+#         self.data = None
+#         self.children = []
+
+#     def add_child(self, obj):
+#         self.children.append(obj)
+
+#     def add_data(self, obj):
+#         self.data = obj
+
+# tree = []
+
+# def my_quick_sort(some_list,node):
+#     n = Node(some_list)
+#     small = []
+#     equal = []
+#     large = []
+#     if len(some_list) > 1:
+#         pivot = some_list[len(some_list)//2]
+#         for i in some_list:
+#             if i > pivot:
+#                 large.append(i)
+#             elif i < pivot:
+#                 small.append(i)
+#             else:
+#                 equal.append(i)
+#         n.add_data(small+equal+large)
+#         n.add_child(small)
+#         n.add_child(equal)
+#         n.add_child(large)
+#         tree.append(n)
+#         return my_quick_sort(small) + my_quick_sort(equal) + my_quick_sort(large)
+#     else:
+#         tree.append(n)
+#         return some_list
+
+# sorted_list = my_quick_sort(create_random_list(50))
+
+# def find_parent(child):
+#     for i in tree:
+#         if child in i.children:
+#             return i.parent
+
+class Node(object):
+    def __init__(self, parent):
+        self.parent = None
+        self.data = None
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+    def add_data(self, obj):
+        self.data = obj
+
+def my_quick_sort(some_list,node):
+    n = Node(node)
+    node.add_child(n)
     small = []
     equal = []
     large = []
@@ -73,24 +166,12 @@ def my_quick_sort(some_list, level=0):
                 small.append(i)
             else:
                 equal.append(i)
-        tree.append({'parent':parent,'data':small})
-        tree.append({'parent':parent,'data':equal})
-        tree.append({'parent':parent,'data':large})
-        parent+=1
-        new_list = my_quick_sort(small,level=level+1) + my_quick_sort(equal,level=level+1) + my_quick_sort(large,level=level+1)
-        #display(some_list) ##Visualising what is happening but not exactly clear
-        #tree.append([level, new_list])
-        return new_list
+        n.add_data(small+equal+large)
+        return my_quick_sort(small,n) + my_quick_sort(equal,n) + my_quick_sort(large,n)
     else:
-        #tree.append([level, some_list])
-        return some_list     
+        n.add_data(some_list)
+        n.add_child(None)
+        return some_list
 
-#my_bubble_sort(create_random_list(250))
-#my_insertion_sort(create_random_list(250))
-#my_selection_sort(create_random_list(250))
-data = create_random_list(50)
-tree.append({'parent':0,'data':data})
-my_quick_sort(data)
-
-for i in tree:
-    
+tree = Node(None)
+sorted_list = my_quick_sort(create_random_list(50),tree)
